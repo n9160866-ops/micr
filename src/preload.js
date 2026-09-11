5a5ce21567b327976d71c2262b79a0eac6babc8f
n9160866-ops <n9160866@gmail.com>
2026-09-11 18:23:01 +0100

    Delete src directory

--- src/preload.js
+++
@@ -1,7 +0,0 @@
-const { contextBridge, ipcRenderer } = require("electron");
-
-contextBridge.exposeInMainWorld("hwAPI", {
-  onData: (callback) => {
-    ipcRenderer.on("hw-data", (_event, data) => callback(data));
-  },
-});
